6d4bcfdab67381a415263364d3bbe727df5d7536
Prof. Ninan Sajeeth Philip, PhD <sajeethphilip@gmail.com>
2025-06-04 03:37:47 +0530

    update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="BSG_IDE",
-    version="4.9",
+    version="4.9.2",
     packages=find_packages(),
     install_requires=[
         "customtkinter==5.2.2",
